8a210dc718f0f93123ea50dccd20112c3258c3f7
tj-tieso <tj.tieso@gmail.com>
2022-06-14 17:26:57 -0500

    post routes functioning

--- models/index.js
+++ models/index.js
@@ -2,4 +2,8 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// define table relationships
+User.hasMany(Post, { foreignKey: "user_id" });
+Post.belongsTo(User, { foreignKey: "user_id" });
+
 module.exports = { User, Post };

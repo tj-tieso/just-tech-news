ea7868cb1dccc1b3e7f3ceefdfcf47e32a6921ac
tj-tieso <tj.tieso@gmail.com>
2022-06-15 13:36:21 -0500

    comment-routes and model

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 // define table relationships
 User.hasMany(Post, { foreignKey: "user_id" });
@@ -40,4 +41,14 @@ Post.hasMany(Vote, {
   foreignKey: "post_id",
 });
 
-module.exports = { User, Post, Vote };
+// COMMENT ASSOCIATIONS
+Comment.belongsTo(User, { foreignKey: "user_id" });
+
+Comment.belongsTo(Post, { foreignKey: "post_id" });
+
+User.hasMany(Comment, { foreignKey: "user_id" });
+
+Post.hasMany(Comment, { foreignKey: "post_id" });
+
+// exports
+module.exports = { User, Post, Vote, Comment };

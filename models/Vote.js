262d22d85b0cda5f2cc99e824ce1156297a3a5d5
tj-tieso <tj.tieso@gmail.com>
2022-06-15 12:44:14 -0500

    voting system

---
+++ models/Vote.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Vote extends Model {}
+
+// When a user votes on a post,
+// insert a new row of data to the table,
+// which lists the primary key of the user
+// and the primary key of the post they voted on.
+
+Vote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "vote",
+  }
+);
+
+module.exports = Vote;
